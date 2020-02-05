0753f4c54ac6a2f3e2d1978ff1f4352f78b50676
Samuel <Samuel.joly@laplateforme.io>
2020-02-05 13:34:39 +0100

    added connect method

--- user.php
+++ user.php
@@ -1,4 +1,15 @@
 <?php
+/*
+	user->register() = Get the user infos and send them to tye Database
+	
+
+
+
+
+*/
+
+
+
 	class user
 	{
 		private $id;
@@ -10,20 +21,57 @@
 		public function register($login, $password, $email, $firstname, $lastname)
 		{
 			if(!empty($login) && !empty($password) && !empty($email) && !empty($firstname) && !empty($lastname))
+			{				
+				$conn = mysqli_connect("localhost","root","","poo");
+				if(empty(mysqli_fetch_row(mysqli_query($conn, "SELECT login FROM utilisateurs WHERE login = '".$login."'"))))
+				{
+					if(mysqli_query($conn, "INSERT INTO `utilisateurs` (`id`, `login`, `email`, `firstname`, `lastname`, `password`)
+											VALUES (NULL, '".$login."', '".$email."', '".$firstname."', '".$lastname."'
+											, '".password_hash($password, PASSWORD_BCRYPT)."')"))
+					{
+						echo "Inscription validée<br>";
+						return [$login, $password, $email, $firstname,$lastname] ;	
+					}
+					else
+					{
+						echo "Erreur des champs sont mal remplis <br>";
+					}
+				}
+				else
+				{
+					echo "Login deja pris <br>";
+				}
+				mysqli_close($conn);
+			}
+		}
+		
+		public function connect($login, $password)
+		{
+			if(!empty($login) && !empty($password))
 			{
-				$this->login = $login;
-				$this->password = $password;
-				$this->email = $email;
-				$this->firstname = $firstname;
-				$this->lastname = $lastname;
-				
-				return( [$this->login, $this->password, $this->email, $this->firstname,$this->lastname] );
+				$conn = mysqli_connect("localhost","root","","poo");
+				$usr_data = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM utilisateurs WHERE login = '".$login."'"));
+				if(password_verify($password,$usr_data["password"]))
+				{
+					$this->id = $usr_data["id"];
+					$this->login = $usr_data["login"];
+					$this->email = $usr_data["email"];
+					$this->firstname = $usr_data["firstname"];
+					$this->lastname = $usr_data["lastname"];
+					
+					echo "Bienvenu ".$this->login." !<br>";
+					return [$this->id, $this->login, $this->email, $this->firstname, $this->lastname];
+				}
+				else
+				{
+					echo "Mauvais mot de passe ou login <br>";
+				}
 			}
 		}
 	}	
 	
 	$enzo = new user();
-	var_dump($enzo->register("0nz3", "0000", "enzo-mandine@laplateforme.io", "enzo", "mandine"));
-	
+	$enzo->register("0nz3", "0000", "enzo-mandine@laplateforme.io", "enzo", "mandine");
+	$enzo->connect("0nz3", "0000");
 	
 ?>

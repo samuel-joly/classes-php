4e55df7c5292740bee48ec44af3d827576668ae4
Samuel <Samuel.joly@laplateforme.io>
2020-02-05 16:39:43 +0100

    Added all the method after update

--- user.php
+++ user.php
@@ -1,7 +1,11 @@
 <?php
 /*
-	user->register() = Get the user infos and send them to tye Database
-	user->connect() = 
+	user->register() 	= Get the user infos and send them to tye Database
+	user->connect() 	= Save the user datas in the user object
+	user->disconnect() 	= Free user attributes 
+	user->delete()		= Free user attributes and delete users data from database
+	user->update()		= Change user data if entered other data than the one saved
+	user->isConnected() = Return true if the user id is not null, false if it is
 
 
 
@@ -96,12 +100,133 @@
 			}
 		}
 		
+		public function update($login, $password , $email , $firstname, $lastname)
+		{
+			$conn = mysqli_connect("localhost","root","","poo");
+			
+			if(!is_null($login))
+			{	
+				if(empty(mysqli_fetch_row(mysqli_query($conn, "SELECT login FROM utilisateurs WHERE login = '".$login."'"))))
+				{
+					if($login != $this->login)
+					{
+						if(mysqli_query($conn, "UPDATE utilisateurs SET login = '".$login."' WHERE id=".$this->id))
+						{
+							echo "Login modifié (ancient: ".$this->login." - nouveau:".$login.")<br>";
+						}
+					}					
+				}
+			}
+			
+			if(!is_null($password))
+			{	
+				$usr_password = mysqli_fetch_row(mysqli_query($conn, "SELECT password FROM utilisateurs WHERE id =".$this->id))[0];
+				if(!password_verify($password, $usr_password))
+				{
+					if(mysqli_query($conn, "UPDATE utilisateurs SET password = '".$password."' WHERE id=".$this->id))
+					{
+						echo "Password modifié <br>";
+					}
+				}
+			}			
+			
+			if(!is_null($email))
+			{			
+				if($email != $this->email)
+				{
+					if(mysqli_query($conn, "UPDATE utilisateurs SET email= '".$email."' WHERE id=".$this->id))
+					{
+						echo "Email modifié (ancient: ".$this->email." - nouveau:".$email.")<br>";
+					}
+				}
+			}			
+			
+			if(!is_null($firstname))
+			{			
+				if($firstname!= $this->firstname)
+				{
+					if(mysqli_query($conn, "UPDATE utilisateurs SET firstname = '".$firstname."' WHERE id=".$this->id))
+					{
+						echo "Firstname modifié (ancient: ".$this->firstname." - nouveau:".$firstname.")<br>";
+					}
+				}
+			}
+			
+			if(!is_null($lastname))
+			{			
+				if($lastname!= $this->lastname)
+				{
+					if(mysqli_query($conn, "UPDATE utilisateurs SET lastname = '".$lastname."' WHERE id=".$this->id))
+					{
+						echo "Lastname modifié (ancient: ".$this->lastname." - nouveau:".$lastname.")<br>";
+					}
+				}
+			}
+		}
 		
+		public function isConnected()
+		{
+			if(!is_null($this->id))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	
+		public function getAllInfos()
+		{
+			return [$this->id, $this->login, $this->email, $this->firstname, $this->lastname];
+		}
+	
+		public function getLogin()
+		{
+			if(!is_null($this->id))
+			{
+				return $this->login;
+			}
+		}
+		
+		public function getEmail()
+		{
+			if(!is_null($this->id))
+			{
+				return $this->email;
+			}
+		}
+		
+		public function getFirstname()
+		{
+			if(!is_null($this->id))
+			{
+				return $this->firstname;
+			}
+		}
+		
+		public function getLastname()
+		{
+			if(!is_null($this->id))
+			{
+				return $this->lastname;
+			}
+		}
+		
+		public function refresh()
+		{
+			if(!is_null($this->id))
+			{
+				$conn = mysqli_connect("localhost","root","","poo");
+				$usr_data = mysqli_fetch_assoc(mysqli_query($conn,"SELECT * FROM utilisateurs WHERE id = ".$this->id));
+				if(!empty($usr_data))
+				{
+					$this->id = $usr_data["login"];
+					$this->email = $usr_data["email"];
+					$this->firstname = $usr_data["firstname"];
+					$this->lastname = $usr_data["lastname"];
+				}				
+			}
+		}
 	}	
-	
-	$enzo = new user();
-	$enzo->register("0nz3", "0000", "enzo-mandine@laplateforme.io", "enzo", "mandine");
-	$enzo->connect("0nz3", "0000");
-	// $enzo->delete();
-	
 ?>
